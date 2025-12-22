c3e464a57191869587c8b7e2e2f9f2ae897ef5f5
Harold Espitia Paz <haroldespitia@MacBook-Pro-de-L-BOG-YJ6M4.local>
2025-12-22 10:12:34 -0500

    Addition of the swagger documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/version')
+  @ApiOperation({ summary: 'Returns the current version of the API' })
+  @ApiResponse({ status: 200, description: 'The current version of the API' })
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getVersion();
   }
 }
--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -11,27 +11,61 @@ import {
 import { RestaurantService } from './restaurant.service';
 import { RestaurantDto } from './dtos/restaurant.dto';
 import { Restaurant } from './schema/restaurant_schema';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('restaurants')
 export class RestaurantController {
   constructor(private restaurantService: RestaurantService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Returns all the restaurants in the DB' })
+  @ApiResponse({ status: 200, description: 'All the restaurants in the DB' })
   public getAllRestaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getRestaurants();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Returns a restaurant identified by the ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The restaurant identified by the ID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'The id of the restaurant',
+  })
   public getRestaurantById(@Param('id') id: string): Promise<Restaurant> {
     return this.restaurantService.getRestaurantById(id);
   }
 
   @Post()
-  public createRestaurant(@Body() body: RestaurantDto) : Promise<Restaurant> {
+  @ApiOperation({ summary: 'Creates a new restaurant in the DB' })
+  @ApiResponse({
+    status: 200,
+    description: 'A new restaurant was created successfully',
+  })
+  @ApiBody({ type: RestaurantDto })
+  public createRestaurant(@Body() body: RestaurantDto): Promise<Restaurant> {
     return this.restaurantService.createRestaurant(body);
   }
 
   @Delete()
+  @ApiOperation({ summary: 'Deletes a restaurant from the DB' })
+  @ApiResponse({
+    status: 200,
+    description: 'The restaurant was deleted successfully',
+  })
+  @ApiQuery({
+    name: 'id',
+    required: true,
+  })
   public async deleteRestaurant(
     @Query('id') id: string,
   ): Promise<Restaurant | null> {
@@ -40,6 +74,17 @@ export class RestaurantController {
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Updates an existing restaurant in the DB' })
+  @ApiResponse({
+    status: 200,
+    description: 'The existing restaurant information was updated successfully',
+  })
+  @ApiBody({ type: RestaurantDto })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'The id of the restaurant',
+  })
   public async updateRestaurant(
     @Param('id') id: string,
     @Body() body: RestaurantDto,
